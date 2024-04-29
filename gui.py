0c7e08f04fd5eeb3512e60bd392e3fd050a30ed6
Garvit-Agrawal7 <garvitagrawal75@gmail.com>
2024-04-29 18:33:43 +0530

    Custom Tkinter layout - 1

--- gui.py
+++ gui.py
@@ -1,62 +1,48 @@
-from tkinter import *
-import customtkinter as ctk
-import time
+from customtkinter import *
+from PIL import Image
 
 
 class App:
     def __init__(self, word_list):
-        self.start_time = None
-        self.expected_text = ""
-        for word in word_list:
-            self.expected_text += word+" "
+        # Initializing Custom Tkinter
+        self.app = CTk
+        self.app.geometry('1280x720')
+        self.app.title('Typing Speed Test')
+        set_appearance_mode('light')
+        self.schedule_function()
 
-        # Initialising Tkinter
-        self.font = ('Poppins', 14)
-        self.screen = Tk()
-        self.screen.title("Typing Speed Test")
-        self.screen.config(padx=100, pady=25)
+        # Word List
+        n = 0
+        for i in range(len(word_list)):
+            n += len(word_list[i])
+            if n * 7 >= 640:
+                word_list.insert(i, "\n")
+                n = 0
+        self.words = " ".join(word_list)
 
-        # Adding keyboard layout
-        self.canvas = Canvas(width=1024, height=768)
-        keyboard = PhotoImage(file='keybr.png')
-        self.canvas.create_image(540, 540, image=keyboard)
-        self.canvas.grid(row=7, column=5, columnspan=5, rowspan=2)
+        # Creating Image
+        img = Image.open('keybr.png')
+        self.image = CTkImage(img, size=img.size)
+        self.my_label = CTkLabel(self.app, text="", image=self.image)
+        self.my_label.place(x=640, y=500, anchor=CENTER)
 
-        # Word list Label
-        self.text_label = Label(self.screen, text=word_list)
-        self.text_label.configure(font=self.font)
-        self.text_label.grid(row=6, column=5)
+        # Typing Test
+        self.entry = CTkEntry(self.app, fg_color="#ebebeb", width=640, height=40, font=('Open Sans', 20))
+        self.label = CTkLabel(self.app, text=self.words, width=640, font=('Open Sans', 20))
+        self.label.place(x=640, y=200, anchor=CENTER)
+        self.entry.place(x=640, y=300, anchor=CENTER)
 
-        # Typing input
-        self.typing_entry = Entry()
-        self.typing_entry.grid(column=5, row=7)
-        self.typing_entry.configure(highlightthickness=2, highlightcolor="#0C0C0C", bg="#F0EBE3", font=self.font,
-                                    width=50)
+        # Progress bar
+        self.progressbar = CTkProgressBar(self.app, orientation="horizontal", width=600, progress_color="#F3D0D7", fg_color="#FAF9F6")
+        self.progressbar.place(x=640, y=100, anchor=CENTER)
 
-        # Start and stop test
-        start_button = Button(self.screen, text="Start Typing Test", command=self.start_typing_test)
-        start_button.grid(column=4, row=8)
-        end_button = Button(self.screen, text="End Typing Test", command=self.end_typing_test)
-        end_button.grid(column=6, row=8)
+        self.app.mainloop()
 
-        # Typing Test result
-        self.feedback_label = Label(self.screen, text="")
-        self.feedback_label.grid(column=5, row=8)
+    def update(self):
+        progress = len(self.entry.get()) / len(self.words)
+        self.progressbar.set(progress)
+        print(progress)
 
-        self.screen.mainloop()
-
-    def start_typing_test(self):
-        """Starts typing test"""
-        self.typing_entry.focus()
-        self.start_time = time.time()
-        self.text_label.config(text=self.expected_text)
-
-    def end_typing_test(self):
-        """Ends typing test"""
-        end_time = time.time()
-        time_taken = end_time - self.start_time
-        input_text = self.typing_entry.get()
-        correct_chars = sum(1 for a, b in zip(input_text, self.expected_text) if a == b)
-        accuracy = (correct_chars / len(self.expected_text)) * 100
-        typing_speed = (len(input_text) / 5) / (time_taken / 60)  # Assuming average word length is 5 characters
-        self.feedback_label.config(text=f"Accuracy: {accuracy:.2f}%\nTyping Speed: {typing_speed:.2f} WPM")
+    def schedule_function(self):
+        self.update()
+        self.app.after(100, self.schedule_function)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gui import App
 
 # Getting the random word list
 words = WordList()
-list_of_words = words.getting_words(sample_size=20)
+list_of_words = words.getting_words(sample_size=30)
 
 # Creating the UI
 app = App(word_list=list_of_words)
